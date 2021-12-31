ed73f6e8bbc7433433f5a8fbfb22247e06946ecd
worldofelisa <worldofelisa@gmail.com>
2021-12-31 16:18:12 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	const faces = 6
+	const min = 1
+	result:=rand.Intn(faces-min)+min
+	fmt.Println (result)
+}
