ad499c6a3f74c4f10f6f9da684aee30f9ec837cc
worldofelisa <worldofelisa@gmail.com>
2022-01-01 16:42:00 +0000

    adding new types of dice and cleaning up the code so it isn't repetitive

--- main.go
+++ main.go
@@ -7,10 +7,30 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	const faces = 6
-	const min = 1
-	result := rand.Intn(faces-min) + min
-	fmt.Println(result)
+	d4Result := RollaDie(4)
+	fmt.Println(d4Result)
 
+	d6Result := RollaDie(6)
+	fmt.Println(d6Result)
+
+	d8Result := RollaDie(8)
+	fmt.Println(d8Result)
+
+	d10Result := RollaDie(10)
+	fmt.Println(d10Result)
+
+	d12Result := RollaDie(12)
+	fmt.Println(d12Result)
+
+	d20Result := RollaDie(20)
+	fmt.Println(d20Result)
+
+	d100Result := RollaDie(100)
+	fmt.Println(d100Result)
+}
+
+func RollaDie(numberofFaces int) (resultOfRoll int) {
+	rand.Seed(time.Now().UnixNano())
+	const min = 1
+	return rand.Intn(numberofFaces-min) + min
 }
