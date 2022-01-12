121e781257fa5981fb7127ab1240c8b92bb1d5e9
worldofelisa <worldofelisa@gmail.com>
2022-01-12 09:53:39 +0000

    refactoring the code to remove the repetitiveness of listing out each type of die and how to roll it. Also making it so in the run command you can say roll x x to roll the amount of dice and the type of dice based on the amount of faces the dice have.

--- main.go
+++ main.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
-	d4Result := RollaDie(4)
-	fmt.Println(d4Result)
+	inputArguments := os.Args
+	numberofDice, err := strconv.Atoi(inputArguments[2])
+	if err != nil {
+		fmt.Println("You need to add a number before the number of faces to say how many dice you want to roll.")
+		fmt.Println("For example: roll 2 10")
+	}
 
-	d6Result := RollaDie(6)
-	fmt.Println(d6Result)
+	numberofFaces, err := strconv.Atoi(inputArguments[3])
+	if err != nil {
+		fmt.Println("You need to add the number of faces on the die.")
+		log.Fatalln(err)
+	}
 
-	d8Result := RollaDie(8)
-	fmt.Println(d8Result)
-
-	d10Result := RollaDie(10)
-	fmt.Println(d10Result)
-
-	d12Result := RollaDie(12)
-	fmt.Println(d12Result)
-
-	d20Result := RollaDie(20)
-	fmt.Println(d20Result)
-
-	d100Result := RollaDie(100)
-	fmt.Println(d100Result)
+	for x := 0; x < numberofDice; x++ {
+		result := RollaDie(numberofFaces)
+		fmt.Println(result)
+	}
 }
 
 func RollaDie(numberofFaces int) (resultOfRoll int) {
@@ -34,3 +34,5 @@ func RollaDie(numberofFaces int) (resultOfRoll int) {
 	const min = 1
 	return rand.Intn(numberofFaces-min) + min
 }
+
+// TODO create an extension for the rollies game, or maybe a sub code
